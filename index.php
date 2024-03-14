27fb6067992ded3435311551baf3f9683f31fdac
Sayyidzf <156924786+Sayyidzf@users.noreply.github.com>
2024-03-14 13:28:50 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wedding-----Planner</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+</head>
+
+<body>
+    <header class="head">
+        <a href="#" class="logo"><i>Wedding</i><i class="us">Us</i></a>
+        <nav class="navbar ">
+            <a href="#" class="active">Home</a>
+            <a href="#service">Service</a>
+            <a href="#vendor">Vendors</a>
+            <a href="#venue">Venue</a>
+            <a href="#invite">E-invites</a>
+            <a href="#contact">Contact</a>
+        </nav>
+        <div id="menu-bar"><i class="fas fa-bars"></i></div>
+    </header>
+    <!---------------------------Home--------------------->
+    <section class="home" id="home">
+        <form action="#">
+            <div class="search-box">
+                <h1>Your Wedding,Your Way</h1>
+                <p>Find the best wedding vendors with thousands of trusted reviews</p>
+                <button class="btn"><a href="/bakso_solo/bakso_solo/login.php" style="color: white;">LOGIN</a></button>
+            </div>
+        </form>
+    </section>
+
+    <!------------------------services-------------------->
+    <section class="service" id="service">
+        <div class="title">
+            <h1><span>S</span>ervice</h1>
+        </div>
+        <div class="services-row">
+            <div class="services-col">
+                <i class="fas fa-book-open"></i>
+                <h2>invitation</h2>
+                <p>Lorem ipsum dolor sit amet, at mei dolore tritani repudiandae.</p>
+            </div>
+            <div class="services-col">
+                <i class="fas fa-camera"></i>
+                <h2>Photography & Video</h2>
+                <p>Lorem ipsum dolor sit amet, at mei dolore tritani repudiandae.</p>
+            </div>
+            <div class="services-col">
+                <i class="fas fa-brush"></i>
+                <h2>Beauty & Makeup</h2>
+                <p>Lorem ipsum dolor sit amet, at mei dolore tritani repudiandae.</p>
+            </div>
+            <div class="services-col">
+                <i class="fab fa-pagelines"></i>
+                <h2>Wedding flowers</h2>
+                <p>Lorem ipsum dolor sit amet, at mei dolore tritani repudiandae.</p>
+            </div>
+            <div class="services-col">
+                <i class="fas fa-birthday-cake"></i>
+                <h2>wedding cake</h2>
+                <p>Lorem ipsum dolor sit amet, at mei dolore tritani repudiandae.</p>
+            </div>
+            <div class="services-col">
+                <i class="fas fa-music"></i>
+                <h2>music band</h2>
+                <p>Lorem ipsum dolor sit amet, at mei dolore tritani repudiandae.</p>
+            </div>
+            <div class="services-col">
+                <i class="fas fa-utensils"></i>
+                <h2>Catering</h2>
+                <p>Lorem ipsum dolor sit amet, at mei dolore tritani repudiandae.</p>
+            </div>
+            <div class="services-col">
+                <i class="fas fa-ring"></i>
+                <h2>Jewellery</h2>
+                <p>Lorem ipsum dolor sit amet, at mei dolore tritani repudiandae.</p>
+            </div>
+        </div>
+    </section>
+    <!-----------------------------vendor------------------->
+    <section class="vendor" id="vendor">
+        <div class="title">
+            <h1><span>F</span>eatured <span>V</span>endor</h1>
+        </div>
+        <div class="vendor-list">
+            <div class="vendor-row">
+                <div class="rate">4.5&nbsp;<i class="fa fa-star" aria-hidden="true"></i></div>
+                <img src="im-1.jpg" alt="img">
+                <h2>Beauty tales by Abel</h2>
+                <p>Bridal Makeup</p>
+                <h3>Rp.5.000.000</h3>
+            </div>
+            <div class="vendor-row">
+                <div class="rate">4.2&nbsp;<i class="fa fa-star" aria-hidden="true"></i></div>
+                <img src="im2.jpg" alt="img">
+                <h2>Flinters Management</h2>
+                <p>Wedding Planner</p>
+                <h3>Rp.12.000.000</h3>
+            </div>
+            <div class="vendor-row">
+                <div class="rate">5.0&nbsp;<i class="fa fa-star" aria-hidden="true"></i></div>
+                <img src="im3.jpg" alt="img">
+                <h2>Wedding Mela</h2>
+                <p>Wedding Decorators</p>
+                <h3>Rp 80.000.000</h3>
+            </div>
+            <div class="vendor-row">
+                <div class="rate">4.1&nbsp;<i class="fa fa-star" aria-hidden="true"></i></div>
+                <img src="im4.jpg" alt="img">
+                <h2>SeventhHeaven Wedding Company</h2>
+                <p>Wedding Decorators-Rental Only</p>
+                <h3>Rp 90.000.000</h3>
+            </div>
+            <div class="vendor-row">
+                <div class="rate">4.3&nbsp;<i class="fa fa-star" aria-hidden="true"></i></div>
+                <img src="im5.jpg" alt="img">
+                <h2>Wedding Mela</h2>
+                <p>Wedding Decorators</p>
+                <h3>Rp 100.000.000</h3>
+            </div>
+            <div class="vendor-row">
+                <div class="rate">4.0&nbsp;<i class="fa fa-star" aria-hidden="true"></i></div>
+                <img src="im6.jpg" alt="img">
+                <h2>Wedding Mela</h2>
+                <p>Wedding Decorators</p>
+                <h3>Rp 150.000.000</h3>
+            </div>
+        </div>
+    </section>
+    <!----------------------------venue Section-------------->
+    <section class="venue" id="venue">
+        <div class="title">
+            <h1><span>V</span>enues</h1>
+        </div>
+        <div class="venue-list">
+            <div class="venue-box">
+                <img src="venue-garut.jpg" alt="img">
+                <div class="venue-info">
+                    <h2>Garut</h2>
+                    <p>Hotel Santika</p>
+                    <a href="https://maps.app.goo.gl/aP4NuhuUPRYiHmeK6"><button class="btn"> More Info</button></a>
+                </div>
+            </div>
+            <div class="venue-box">
+                <img src="venue-bandung.jpg" alt="img">
+                <div class="venue-info">
+                    <h2>Bandung</h2>
+                    <p>Savoy Homann</p>
+                   <a href="https://maps.app.goo.gl/6f6TWj6oorwNJqRK6"> <button class="btn">More Info</button></a>
+                </div>
+            </div>
+            <div class="venue-box">
+                <img src="venue-jakarta.jpg" alt="img">
+                <div class="venue-info">
+                    <h2>Jakarta</h2>
+                    <p>The Jayakarta SP</p>
+                   <a href="https://maps.app.goo.gl/v3CxBHj2AiJrd83z6"> <button class="btn">More Info</button></a>
+                </div>
+            </div>
+            <div class="venue-box">
+                <img src="venue-bekasi.jpg" alt="img">
+                <div class="venue-info">
+                    <h2>Bekasi</h2>
+                    <p>Ibis Styles</p>
+                    <a href="https://maps.app.goo.gl/a1Xd8nkfwh5DD1HN8"><button class="btn">More Info</button></a>
+                </div>
+            </div>
+            <div class="venue-box">
+                <img src="venue-bogor.jpg" alt="img">
+                <div class="venue-info">
+                    <h2>Bogor</h2>
+                    <p>Swiss-Belcourt</p>
+                    <a href="https://maps.app.goo.gl/Tu7hQTHf4a5SUrS88"><button class="btn">More Info</button></a>
+                </div>
+            </div>
+            <div class="venue-box">
+                <img src="venue-tangerang.jpg" alt="img">
+                <div class="venue-info">
+                    <h2>Tangerang</h2>
+                    <p>Golden Tulip Essential</p>
+                   <a href="https://maps.app.goo.gl/sHM4VtCmxbTnPrEj8"> <button class="btn">More Info</button></a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!------------------E-invitation------------------>
+    <section class="invite" id="invite">
+        <div class="title">
+            <h1>Card<span>Design</span></h1>
+            <p>Choose the best card Design.</p>
+        </div>
+        <div class="invitation-row">
+            <div class="invitation-box">
+                <img src="card-1.jpg" alt="">
+            </div>
+            <div class="invitation-box">
+                <img src="card-2.jpg" alt="">
+            </div>
+            <div class="invitation-box">
+                <img src="card-3.jpg" alt="">
+            </div>
+            <div class="invitation-box">
+                <img src="card-4.jpg" alt="">
+            </div>
+            <div class="invitation-box">
+                <img src="card-5.jpg" alt="">
+            </div>
+            <div class="invitation-box">
+                <img src="card-6.jpg" alt="">
+            </div>
+            <div class="invitation-box">
+                <img src="card-7.jpg" alt="">
+            </div>
+            <div class="invitation-box">
+                <img src="card-8.jpg" alt="">
+            </div>
+            <div class="invitation-box">
+                <img src="card-9.jpg" alt="">
+            </div>
+            <div class="invitation-box">
+                <img src="card-10.jpg" alt="">
+            </div>
+            <div class="invitation-box">
+                <img src="card-11.jpg" alt="">
+            </div>
+            <div class="invitation-box">
+                <img src="card-12.jpg" alt="">
+            </div>
+        </div>
+    </section>
+
+    <!--------------------------------------------footer section--------------------------->
+    <footer id="contact">
+        <div class="container">
+            <div class="f-container">
+                <h2>WedMeGood - Your Personal Wedding Planner</h2><br>
+                <p>Plan your wedding with Us WedMeGood is an Indian Wedding Planning Website </p>
+                <div>
+                    <button class="btn">Register as a Vendor</button>
+                </div>
+                <div class="social">
+                    <a href=""><i class="fab fa-facebook"></i></a>
+                    <a href="https://www.instagram.com/z_ways"><i class="fab fa-instagram"></i></a>
+                    <a href=""><i class="fab fa-linkedin"></i></a>
+                    <a href=""><i class="fab fa-twitter"></i></a>
+                    <a href=""><i class="fab fa-pinterest"></i></a>
+                </div>
+            </div>
+            <div class="f-container">
+                <h2>Our Location</h2>
+                <a href="#venue"><i class="fas fa-angle-right"></i>Garut</a>
+                <a href="#venue"><i class="fas fa-angle-right"></i>Bandung</a>
+                <a href="#venue"><i class="fas fa-angle-right"></i>Jakarta</a>
+                <a href="#venue"><i class=" fas fa-angle-right "></i>Bekasi</a>
+                <a href="#venue"><i class="fas fa-angle-right "></i>Bogor</a>
+                <a href="#venue"><i class="fas fa-angle-right "></i>Tangerang</a>
+                </div>
+                
+                <div class="f-container ">
+                    <h2>Our Newsletter</h2>
+                    <p>Subscribe for latest updates</p>
+                    <input type="text " placeholder="Enter Your Email ">
+                    <button class="btn ">Subscribe</button>
+                    </div>
+            </div>
+        </footer>
+        <div class="copyright ">
+            <p>Copyright ©2023 All Rights Reserved | Kelompok 2</p>
+        </div>
+</body>
+<script>
+     let menu = document.querySelector('#menu-bar');
+    let head = document.querySelector('.head .navbar');
+
+    menu.onclick = () => {
+        head.classList.toggle('active');
+    };
+
+    window.onscroll = () => {
+        head.classList.remove('active');
+        if (window.scrollY > 60) {
+            document.querySelector('#menu-bar').classList.add('active');
+        } else {
+            document.querySelector('#menu-bar').classList.remove('active');
+        }
+    };
+</script>  
